2e18f1789f8c274d26506354cde9fb6616355bba
jo-gay <jo.gay@ndm.ox.ac.uk>
2025-05-15 16:13:13 +0100

    feat: tweak difficulty settings

--- src/antibiotic.gd
+++ src/antibiotic.gd
@@ -13,8 +13,8 @@ var colours: Dictionary
 @export var icon_green: Resource
 var icons: Dictionary
 
-@export var res_contribution: float = 0.04
-@export var misuse_res_contribution: float = 0.1
+@export var res_contribution: float
+@export var misuse_res_contribution: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -44,11 +44,11 @@ func update_visuals():
 	$Sprite2D/Symbol.texture = icons[type]
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
-	get_tree().root.get_node("Main/BugSpawner").increase_resistance(type, misuse_res_contribution)
+	get_tree().root.get_node("Main/BugSpawner").increase_resistance(type, Global.misuse_res_contribution)
 	queue_free()
 
 
 func _on_area_entered(area: Area2D) -> void:
-	get_tree().root.get_node("Main/BugSpawner").increase_resistance(type, res_contribution)
+	get_tree().root.get_node("Main/BugSpawner").increase_resistance(type, Global.res_contribution)
 	area.take_dmg(1, type)
 	queue_free()
