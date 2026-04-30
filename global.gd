3e2a76c0608e711bec3220b28d8b984303c47055
bobturneruk <bob.turner.uk@gmail.com>
2026-04-30 11:05:20 +0100

    feat: avatar selection

--- global.gd
+++ global.gd
@@ -5,3 +5,14 @@ var spawn_rate_acceleration = 0.02
 var initial_resistance_rate = 0
 var misuse_res_contribution = 0.04
 var res_contribution = 0.01
+var avatar_index: int
+
+var avatars = [
+	load("res://art/avatars/angry_pasteur.svg"),
+	load("res://art/avatars/circle_avatar.svg"),
+	load("res://art/avatars/generic_doctor.svg")
+	]
+
+func _ready():
+	# Randomise avatar when game starts
+	Global.avatar_index = randi_range(0, avatars.size() - 1)
--- src/splash_screen.gd
+++ src/splash_screen.gd
@@ -1,5 +1,8 @@
 extends TextureRect
 
+func _ready():	
+	$avatar_sprite.texture = Global.avatars[Global.avatar_index]
+
 func _on_button_pressed() -> void:
 	var difficulty = get_node("DifficultyButton").get_selected_id()
 	match difficulty:
@@ -25,3 +28,18 @@ func _on_button_pressed() -> void:
 			Global.res_contribution = 0.05
 			Global.misuse_res_contribution = 0.15
 	get_tree().change_scene_to_file("res://scenes/single_player.tscn")
+
+
+func _on_pick_left_pressed() -> void:
+	if Global.avatar_index - 1 >= 0:
+		Global.avatar_index = Global.avatar_index - 1
+	else:
+		Global.avatar_index = Global.avatars.size() - 1
+	$avatar_sprite.texture = Global.avatars[Global.avatar_index]
+
+func _on_pick_right_pressed() -> void:
+	if Global.avatar_index + 1 < Global.avatars.size():
+		Global.avatar_index = Global.avatar_index + 1
+	else:
+		Global.avatar_index = 0
+	$avatar_sprite.texture = Global.avatars[Global.avatar_index]
